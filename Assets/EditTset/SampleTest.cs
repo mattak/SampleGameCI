51af159d26ca9367fd4021a6a98233c3b6c5a2ed
mattak <mattak.me@gmail.com>
2021-12-17 22:51:55 +0900

    :seedling: sample test to fail

--- Assets/EditTset/SampleTest.cs
+++ Assets/EditTset/SampleTest.cs
@@ -1,43 +1,13 @@
-using System.IO;
-using System.Text;
 using NUnit.Framework;
-using UnityEngine;
-using YamlDotNet.RepresentationModel;
 
 namespace Sample
 {
     public class SampleTest
     {
         [Test]
-public void ParseTest()
-{
-    // setup
-    var text = @"
-numbers:
-  - 1
-  - 2
-  - 3
-HP: 100
-MP: 10
-SP: 32
-";
-    var yamlPath = "/tmp/sample.yml";
-    File.WriteAllText(yamlPath, text);
-
-    // read
-    var reader = new StreamReader(yamlPath, Encoding.UTF8);
-    var yaml = new YamlStream();
-    yaml.Load(reader);
-
-    var numbersNode = yaml.Documents[0].RootNode["numbers"];
-    Debug.Log(numbersNode); // Output: [ 1, 2, 3 ]
-    
-    var hpNode = yaml.Documents[0].RootNode["HP"];
-    Debug.Log(hpNode); // Output: 100
-    var mpNode = yaml.Documents[0].RootNode["MP"];
-    Debug.Log(mpNode); // Output: 10
-    var spNode = yaml.Documents[0].RootNode["SP"];
-    Debug.Log(spNode); // Output: 32
-}
+        public void ParseTest()
+        {
+            Assert.AreEqual(1 + 1, 1, "must be same");
+        }
     }
 }
